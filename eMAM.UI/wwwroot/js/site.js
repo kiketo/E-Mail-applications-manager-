82fb4fc2032f1ee279f937e2d0a7817cf30c5641
Kiril Kuzmanov <kiketo@gmail.com>
2019-06-06 14:16:16 +0300

    start work on Ajax

--- eMAM.UI/wwwroot/js/site.js
+++ eMAM.UI/wwwroot/js/site.js
@@ -1,1 +1,7 @@
-﻿
+﻿$('.email-button').click(function (ev) {
+    var currentLink = $(ev.target);
+    console.log('catch event');
+
+    var gmailId = currentLink.data('target');//remove "#mails-"
+
+});
