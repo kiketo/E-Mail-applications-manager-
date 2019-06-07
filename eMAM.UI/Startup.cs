c0b097e13f4428067a7b58c38d9b3d896ef95be4
rhin <rhin3381@gmail.com>
2019-06-07 19:40:12 +0300

    logs implemented (somewhat), kendo removed

--- eMAM.UI/Startup.cs
+++ eMAM.UI/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using eMAM.Data;
 using eMAM.Data.Models;
+using eMAM.Logs.Extensions;
 using eMAM.Service.Utills;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -8,8 +9,11 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Serialization;
 using System.Reflection;
+using log4netConfiguration = eMAM.Logs.Appenders.Configuration;
+
 
 namespace eMAM.UI
 {
@@ -87,8 +91,22 @@ namespace eMAM.UI
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+
+            string connectionString = Configuration["Logging:ConnectionString"];
+            string logFilePath = Configuration["Logging:LogFilePath"];
+
+            loggerFactory.AddLog4Net(new[]
+            {
+                log4netConfiguration.CreateConsoleAppender(),
+                log4netConfiguration.CreateRollingFileAppender(logFilePath),
+                log4netConfiguration.CreateTraceAppender(),
+                log4netConfiguration.CreateAdoNetAppender(connectionString)
+            });
+
+           // DBInitializer.Initialize(context);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
