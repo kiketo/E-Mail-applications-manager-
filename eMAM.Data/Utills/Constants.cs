f90882d179b0a3c1685a447013af13763d90a093
Kiril Kuzmanov <kiketo@gmail.com>
2019-06-14 09:20:25 +0300

    .

---
+++ eMAM.Data/Utills/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eMAM.Data.Utills
+{
+    public class Constants
+    {
+        public const string posgrePassword = "kikcho";
+
+        public const string serverName = "ADMIN-PC34";
+    }
+}
